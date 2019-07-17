79f3b8a9dbcdcf638670e164cf4f3afff855abbc
Sid Mallya <48600731+SidMallya@users.noreply.github.com>
2019-07-17 15:31:07 +1000

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,10 @@
 <?php
-	$endpoint = "hostname-or-endpoint";
-	$DB = "photo_sharing";
-	$user = "db-username";
-	$pwd = "db-password";
-	$bucket = "s3-bucket-name";
-	$region = "region-eg-us-east-1";
+
+$endpoint = "hostname-or-endpoint";
+$DB = "photo_sharing";
+$user = "db-username";
+$pwd = "db-password";
+$bucket = "s3-bucket-name";
+$region = "region-eg-us-east-1";
+
 ?>
